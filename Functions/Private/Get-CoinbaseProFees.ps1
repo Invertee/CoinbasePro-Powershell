360487f476a331394e815a0a71ec53c443135649
Sam Petch <smpetch+github@gmail.com>
2019-07-27 23:50:56 +0100

    Function for retreiving fees

---
+++ Functions/Private/Get-CoinbaseProFees.ps1
@@ -0,0 +1,20 @@
+function Get-CoinbaseProFees { 
+        
+    Param(
+    [Parameter(Mandatory=$true)] $APIKey,
+    [Parameter(Mandatory=$true)] $APISecret,
+    [Parameter(Mandatory=$true)] $APIPhrase,
+    [parameter()] [switch] $SandboxAPI
+    )
+    
+    $api = Get-BlankAPI -SandboxAPI:$SandboxAPI
+    $api.key = "$APIKey"
+    $api.secret = "$APISecret"
+    $api.passphrase = "$APIPhrase"
+
+    $api.method = 'GET'
+    $api.url = '/fees'
+    $response = Invoke-CoinbaseProRequest $api
+    Write-Output $response
+
+}

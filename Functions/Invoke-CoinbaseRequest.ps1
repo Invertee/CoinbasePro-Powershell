95520344d8ae26639066ee120c0653b3e2b9c967
Sam Petch <smpetch+github@gmail.com>
2021-01-16 13:48:33 +0000

    Support for different locales and formatting

--- Functions/Invoke-CoinbaseRequest.ps1
+++ Functions/Invoke-CoinbaseRequest.ps1
@@ -1,53 +1,49 @@
 function Invoke-CoinbaseProRequest {
-
     Param (
-    [Parameter()] $Request    
+        [Parameter()] $Request    
     )
 
     $EpochStart = Get-Date -Date "01/01/1970"
-    $Now = Get-Date
-    $Timestamp = (New-TimeSpan -Start $EpochStart -End $Now.ToUniversalTime()).TotalSeconds
-    $Timestamp = ([math]::Round($Timestamp, 3)).ToString()
+    $Timestamp = (New-TimeSpan -Start $EpochStart -End (Get-Date).ToUniversalTime()).TotalSeconds
+    $Timestamp = ([math]::Round($Timestamp, 0)).ToString()
     $Prehash = $Timestamp + $request.method.ToUpper() + $request.url + $request.body
     $Signature_b64 = Get-HMAC -Message $prehash -Secret $request.secret
     $Header = @{
-        "CB-ACCESS-KEY" = $request.key
-        "CB-ACCESS-SIGN" = $signature_b64
-        "CB-ACCESS-TIMESTAMP" = $Timestamp
+        "CB-ACCESS-KEY"        = $request.key
+        "CB-ACCESS-SIGN"       = $signature_b64
+        "CB-ACCESS-TIMESTAMP"  = $Timestamp
         "CB-ACCESS-PASSPHRASE" = $request.passphrase
-        "Content-Type" = 'application/json'
-        "accept" = 'application/json'
+        "Content-Type"         = 'application/json'
+        "accept"               = 'application/json'
     }
     $Uri = $request.endpoint + $request.url
     if ($request.method.ToUpper() -eq 'POST') {
-        try {$response = Invoke-RestMethod -Method $request.method -Uri $uri -Headers $header -Body $request.body}
-        catch {$Statuscode = $_.exception.message}
+        try { $response = Invoke-RestMethod -Method $request.method -Uri $uri -Headers $header -Body $request.body }
+        catch { $Statuscode = $_.exception.message }
     } else {
-        try {$response = Invoke-RestMethod -Method $request.method -Uri $uri -Headers $header}
-        catch {$Statuscode = $_.exception.message}
+        try { $response = Invoke-RestMethod -Method $request.method -Uri $uri -Headers $header }
+        catch { $Statuscode = $_.exception.message }
     }
         
     if ($Statuscode) {
-            $Errorcode = $Statuscode | Select-String -Pattern '\d{3,3}'
-            $Errorcode = $Errorcode.Matches.Value
+        $Errorcode = $Statuscode | Select-String -Pattern '\d{3,3}'
+        $Errorcode = $Errorcode.Matches.Value
             
-        Switch ($Errorcode) 
-            {
-            '400' {Write-Error "Bad Request. Invalid request format"}
-            '401' {Write-Error "Unauthorized. Invalid API Key"}
-            '403' {Write-Error "Forbidden. You do not have access to the requested resource"} 
-            '404' {Write-Error "Not Found"}
-            '500' {Write-Error "Internal Server Error"} 
-            }
-            
+        Switch ($Errorcode) {
+            '400' { Write-Error "Bad Request. Invalid request format" }
+            '401' { Write-Error "Unauthorized. Invalid API Key" }
+            '403' { Write-Error "Forbidden. You do not have access to the requested resource" } 
+            '404' { Write-Error "Not Found" }
+            '500' { Write-Error "Internal Server Error" } 
+        }
     }
     Return $response
 }
 
 function Get-HMAC {
     Param(
-    [Parameter()] $Message,
-    [Parameter()] $Secret    
+        [Parameter()] $Message,
+        [Parameter()] $Secret    
     )
 
     $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
@@ -58,18 +54,17 @@ function Get-HMAC {
 }
 
 Function Get-BlankAPI {
-
     Param(
         [Parameter()] [switch] $SandboxAPI   
-        )
+    )
 
     $api = @{
-        "endpoint" = 'https://api.pro.coinbase.com'
-        "url" = ''
-        "method" = ''
-        "body" = ''
-        "key" = ''
-        "secret" = ''
+        "endpoint"   = 'https://api.pro.coinbase.com'
+        "url"        = ''
+        "method"     = ''
+        "body"       = ''
+        "key"        = ''
+        "secret"     = ''
         "passphrase" = ''
     }
 
@@ -80,10 +75,9 @@ Function Get-BlankAPI {
 }
 
 Function Test-Product {
-
     Param(
-        [Parameter(Position=0)] [string] $Product   
-        )
+        [Parameter(Position = 0)] [string] $Product   
+    )
 
     $ValidProducts = Import-Csv "$([system.io.path]::GetTempPath())/CoinbaseProPS-Products.csv"
 
@@ -93,14 +87,12 @@ Function Test-Product {
         Throw "Invalid Product!"
         Return $false
     }
-
 }
 
 Function Test-Currency {
-
     Param(
-        [Parameter(Position=0)] [string] $Currency   
-        )
+        [Parameter(Position = 0)] [string] $Currency   
+    )
 
     $ValidCurrencies = Import-Csv "$([system.io.path]::GetTempPath())/CoinbaseProPS-currencies.csv"
 
@@ -110,5 +102,4 @@ Function Test-Currency {
         Throw "Invalid Currency!"
         Return $false
     }
-    
 }
